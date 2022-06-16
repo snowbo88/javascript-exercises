2d72c44745a8ebdac00186b3b44978ba4c111e8e
Bo <boyadeng@gmail.com>
2022-06-15 23:14:00 -0400

    Passes First Test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, element) {
+    let position = array.indexOf(element);
+    array.splice(position, 1);
+    return array;
 };
 
 // Do not edit below this line
